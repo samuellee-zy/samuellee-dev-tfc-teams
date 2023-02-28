f029f1dc97e65a45aa8030a952fe5e762023d29c
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-03-01 10:44:56 +1100

    test11

--- teams_access.tf
+++ teams_access.tf
@@ -1,14 +1,15 @@
 locals {
   workspace_ids_list = values(nonsensitive(data.tfe_outputs.samuellee-dev-workspace-outputs.values))
+  team_ids           = [for team in tfe_team : team.id]
 }
-
-# output "test" {
-#   value = local.workspace_ids_list
-# }
 
 resource "tfe_team_access" "devs-access-all-workspaces" {
   for_each     = toset(local.workspace_ids_list)
   workspace_id = each.key
   team_id      = tfe_team.developers.id
   access       = "read"
+}
+
+output "test" {
+  value = local.team_ids
 }
