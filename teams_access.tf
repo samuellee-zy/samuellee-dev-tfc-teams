dfbc2e9a6ac14b527300770fbbd863281ec5756c
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-03-01 10:18:11 +1100

    test4

--- teams_access.tf
+++ teams_access.tf
@@ -1,19 +1,10 @@
-# resource "tfe_team_access" "hcp-deployment" {
-#   for_each = {
-#     for index, workspace in data.tfe_outputs.samuellee-dev-workspace-outputs.values :
-#     workspace_id => workspace
-#   }
-#   team_id = tfe_team.developers.id
-# }
-
-
 locals {
   workspaceIds = [data.tfe_outputs.samuellee-dev-workspace-outputs.values]
 }
 
-output "test" {
-  value = [
-    for test in local.workspaceIds : length(test)
-  ]
-  sensitive = true
+resource "tfe_team_access" "hcp-deployment" {
+  for_each     = toset(local.workspaceIds)
+  workspace_id = each.key
+  team_id      = tfe_team.developers.id
+  access       = "read"
 }
