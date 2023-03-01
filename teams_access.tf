508baa70659a73fdf57240e43184c14b92c02bcf
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-03-01 11:39:53 +1100

    fixed devs-access-all-workspaces

--- teams_access.tf
+++ teams_access.tf
@@ -1,6 +1,5 @@
 locals {
   workspace_ids_list = values(nonsensitive(data.tfe_outputs.samuellee-dev-workspace-outputs.values))
-  team_ids           = [for team in tfe_team : team.id]
 }
 
 resource "tfe_team_access" "devs-access-all-workspaces" {
@@ -8,8 +7,4 @@ resource "tfe_team_access" "devs-access-all-workspaces" {
   workspace_id = each.key
   team_id      = tfe_team.developers.id
   access       = "read"
-}
-
-output "test" {
-  value = local.team_ids
 }
